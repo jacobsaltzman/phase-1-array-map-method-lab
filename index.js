1adb0b1ba3219faa4cf774e70cb905080389c643
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-10 21:13:39 -0700

    commit tree b7f869db28ec3d816df243812a5525ab376e5c1f

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(" ")
+    return words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ")
+  })
 }
